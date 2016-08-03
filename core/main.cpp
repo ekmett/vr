e6b1640ae88de2207602c7cd6c00e652ce3fd3ba
Edward Kmett <Edward Kmett>
2016-08-03 06:39:57 -0400

    working towards basic linking

---
+++ core/main.cpp
@@ -0,0 +1,22 @@
+#include <SDL.h>
+#include <GL/glew.h>
+// #include <SDL_opengl.h>
+#include <gl/glu.h>
+#include <stdio.h>
+#include <string>
+#include <cstdlib>
+
+#include <openvr.h>
+
+#if defined(POSIX)
+#include "unistd.h"
+#endif
+
+int main(int argc, char *argv[]) {
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) < 0)
+	{
+		printf("%s - SDL could not initialize! SDL Error: %s\n", __FUNCTION__, SDL_GetError());
+		return false;
+	}
+	return 0;
+}
