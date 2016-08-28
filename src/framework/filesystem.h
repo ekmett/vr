f34a997b1e47a91c675f893c87cadc78c76590f7
Edward Kmett <ekmett@gmail.com>
2016-08-28 06:10:18 -0400

    track filesystem.h

---
+++ src/framework/filesystem.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <boost/filesystem.hpp>
+
+//#ifdef _WIN32
+//#pragma comment(lib, "boost_filesystem-vc140-mt-gd-1_61")
+//#endif
+
+namespace framework {
+  namespace filesystem {
+    using namespace boost::filesystem;
+  };
+};
