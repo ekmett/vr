86345b62d52274f57b79fce56151fa53bbed2495
Edward Kmett <ekmett@gmail.com>
2016-09-13 20:27:24 -0400

    timing is optional

--- timer.h
+++ timer.h
@@ -6,6 +6,8 @@
 #include "spdlog.h"
 #include "noncopyable.h"
 
+// #define TIMING
+
 namespace framework {
 
   typedef duration<GLuint64, std::nano> timestamp;
@@ -16,29 +18,38 @@ namespace framework {
     static const int N = 3; // frames worth of lag between query_frame and current_frame to ensure timers clear
 
     static void start_frame() {
+#ifdef TIMING
       current_frame = (current_frame + 1) % N;
       if (++query_frame >= 0) query_frame %= N;
       log("timer")->info("current frame: {}, query frame: {}", current_frame, query_frame);
+#endif
     }
 
     GLuint timestamps[N];
 
     timer(const char * name) {
+#ifdef TIMING
       glGenQueries(N, timestamps);
       for (int i = 0;i < N;++i)
         gl::label(GL_QUERY, timestamps[i], "timer {} query {}", name, i);
+#endif
     }
     timer(string name) : timer(name.c_str()) {}
 
     ~timer() {
+#ifdef TIMING
       glDeleteQueries(N, timestamps);
+#endif
     }
 
     void tick() const {
+#ifdef TIMING
       glQueryCounter(timestamps[current_frame], GL_TIMESTAMP); // start counter i
+#endif
     }
 
     timestamp tock() const {
+#ifdef TIMING
       GLuint64 result = 0;
       glQueryCounter(timestamps[current_frame], GL_TIMESTAMP); // start counter i
       if (query_frame >= 0) {
@@ -52,6 +63,9 @@ namespace framework {
         }
       }
       return std::chrono::nanoseconds(result);
+#else
+      return std::chrono::nanoseconds(0);
+#endif
     }
   };
 
@@ -69,21 +83,28 @@ namespace framework {
     static int indentation;
     static bool squelch;
 
-    timer_block(const elapsed_timer & t) : t(t), was_squelched(squelch), pop(false) {
+    timer_block(const elapsed_timer & t) 
+#ifdef TIMING
+      : t(t), was_squelched(squelch), pop(false) {
       t.start.tick();
       float ms = (t.end.tock() - t.start.tock()).count() / 1000000.0f;
       if (!squelch) {
         pop = gui::TreeNode(t.name.c_str(),"%s: %.02fms",t.name.c_str(), ms);
         squelch = !pop;
-      }
-    }
+      } 
+#endif
+    {}
     ~timer_block() {
+#ifdef TIMING
       if (pop) gui::TreePop();
       squelch = was_squelched;
       t.end.tick();
+#endif
     }
+#ifdef TIMING
     const elapsed_timer & t;
     bool pop;
     bool was_squelched;
+#endif
   };
 }
