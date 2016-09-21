e8c28016f195ca95b2ecfd89abb3839334063563
Edward Kmett <ekmett@gmail.com>
2016-09-21 11:07:31 -0400

    Allow rank1 and rank0 to accept uimage1D source rather than just textures. Add lod support to rank1 and rank0.

--- shaders/succinct.glsl
+++ shaders/succinct.glsl
@@ -5,24 +5,55 @@
 // (pseudo)-succinct indexed dictionaries
 // --------------------------------------
 
-// s is a 1d GL_RG8UI, GL_RG16UI or GL_RG32UI texture. 
+// s is a 1d GL_RG8UI, GL_RG16UI or GL_RG32UI image. 
 //
 // The red channel contains prefix sums to the start of each block. blue channel contains raw bits.
 //
-// bits is 8, 16 or 32 depending on if we're using a 8, 16 or 32 bit texture
+// bits is 8, 16 or 32 depending on if we're using a 8, 16 or 32 bit image
 //
 // returns the prefix sum of the number of 1 bits up to but not including the i'th bit in the G bit vector.
 //
 // This naive encoding has 2x overhead, but that is still better than 8x-32x overhead caused 
 // by storing indices!
 
-uint rank1(usampler1D s, uint i, uint bits) {
-  uvec2 p = texelFetch(s,int(i>>bits),0).rg;
+uint rank1(usampler1D s, uint i, uint bits, int lod) {
+  uvec2 p = texelFetch(s, int(i >> bits), lod).rg;
+  return p.r + bitCount(p.g&((1 << (i&(bits - 1))) - 1));
+}
+
+uint rank1(layout(rg32ui) uimage1D s, uint i) {
+  const uint bits = 32;
+  uvec2 p = imageLoad(s, int(i >> bits)).rg;
+  return p.r + bitCount(p.g&((1 << (i&(bits - 1))) - 1));
+}
+
+uint rank1(layout (rg16ui) uimage1D s, uint i) {
+  const uint bits = 16;
+  uvec2 p = imageLoad(s,int(i>>bits)).rg;
   return p.r + bitCount(p.g&((1<<(i&(bits-1)))-1));
 }
 
-uint rank0(usampler1D s, uint i, uint bits) {
-  return i - rank1(s, i, bits);
+uint rank1(layout(rg8ui) uimage1D s, uint i) {
+  const uint bits = 8;
+  uvec2 p = imageLoad(s, int(i >> bits)).rg;
+  return p.r + bitCount(p.g&((1 << (i&(bits - 1))) - 1));
 }
+
+uint rank0(usampler1D s, uint i, uint bits, int lod) {
+  return i - rank1(s, i, bits, lod);
+}
+
+uint rank0(layout(rg32ui) uimage1D s, uint i) {
+  return i - rank1(s, i);
+}
+
+uint rank0(layout(rg16ui) uimage1D s, uint i) {
+  return i - rank1(s, i);
+}
+
+uint rank0(layout(rg8ui) uimage1D s, uint i) {
+  return i - rank1(s, i);
+}
+
 
 #endif
