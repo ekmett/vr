7c8fc6b0ebe5128f537654dba529862a35002230
Edward Kmett <ekmett@gmail.com>
2016-09-21 21:50:05 -0400

    aabb/sphere overlaps test, add aabb corner extraction

--- shaders/aabb.glsl
+++ shaders/aabb.glsl
@@ -29,6 +29,26 @@ bool overlaps(vec3 p, aabb b) {
   return overlaps(b, p);
 }
 
+bool overlaps(aabb b, sphere s) {
+  vec3 c = s.data.xyz;
+  float r = s.data.w;
+  return quadrance(
+    mix(
+      mix(
+        vec3(0),
+        c - b.hi,
+        greaterThan(c, b.hi)
+      ),
+      c - b.lo,
+      lessThan(c, b.lo)
+    )
+  ) < r * r;
+}
+
+bool overlaps(sphere s, aabb b) {
+  return overlaps(b, s);
+}
+
 aabb union_aabb(aabb a, aabb b) {
   return aabb(min(a.lo, b.lo), max(a.hi, b.hi));
 }
@@ -39,6 +59,10 @@ aabb union_aabb(aabb a, vec3 p) {
 
 aabb union_aabb(aabb a, sphere s) {
   return union_aabb(a, make_aabb(s));
+}
+
+vec3 corner(aabb b, int i) {
+  return mix(b.lo, b.hi, bvec3((i & 1) == 1, (i & 2) == 2, (i & 4) == 4));
 }
 
 #endif
